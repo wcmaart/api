270cd4c085d0425f2a8fa69631c2122141ebba5f
revdancatt <github@revdancatt.com>
2018-05-29 17:45:59 +0100

    Skeleton Site

--- server.js
+++ server.js
@@ -43,30 +43,30 @@ console.log(`server.js exists in this directory: ${rootDir}`.help)
  * the port, host, environment and if we want to skip any build steps
  */
 const argOptionDefinitions = [{
-    name: 'port',
-    alias: 'p',
-    type: Number
-  },
-  {
-    name: 'host',
-    alias: 'h',
-    type: String
-  },
-  {
-    name: 'env',
-    alias: 'e',
-    type: String
-  },
-  {
-    name: 'skipBuild',
-    alias: 's',
-    type: Boolean,
-    defaultOption: false
-  },
-  {
-    name: 'skipOpen',
-    type: Boolean
-  }
+  name: 'port',
+  alias: 'p',
+  type: Number
+},
+{
+  name: 'host',
+  alias: 'h',
+  type: String
+},
+{
+  name: 'env',
+  alias: 'e',
+  type: String
+},
+{
+  name: 'skipBuild',
+  alias: 's',
+  type: Boolean,
+  defaultOption: false
+},
+{
+  name: 'skipOpen',
+  type: Boolean
+}
 ]
 const commandLineArgs = require('command-line-args')
 const argOptions = commandLineArgs(argOptionDefinitions)
@@ -244,3 +244,196 @@ if (skipBuild === false) {
     sassResult.css
   )
 }
+
+// ########################################################################
+/*
+ * STEP FOUR (optional)
+ *
+ * If we are in developement mode we want to watch for file changes that
+ * mean we need to either recompile source code which requires a restart
+ * of the server. Or recompile CSS or copy over new html/public files
+ * both of which don't require a server restart.
+ *
+ * Bonus, we want to try and _only_ recompile/copy over changed or new
+ * files
+ *
+ */
+if (process.env.NODE_ENV === 'development') {
+  const devtools = require('./app/modules/devtools')
+  devtools.watcher()
+}
+
+// ########################################################################
+/*
+ * STEP FIVE
+ *
+ * Now we have enough for our server to actually run on a port we need
+ * to check to see if a config.json file exist, which is going to actually
+ * hold all the other information.
+ *
+ * Specifically in this case the Auth0 settings as we are now running
+ * the server on we assume either localhost _or_ a public website.
+ * Because Auth0 should be protecting us from all the admin stuff and
+ * initially that isn't in place we need to somehow project the Auth0
+ * form. We'll do this by creating a local token which has to be added
+ * to be able to update the form. The user installing this app will know
+ * the token becasue we'll tell them. But a remote user won't have that
+ * information.
+ * */
+
+const express = require('express')
+const exphbs = require('express-handlebars')
+const routes = require('./app/routes')
+const bodyParser = require('body-parser')
+const cookieParser = require('cookie-parser')
+const session = require('express-session')
+const FileStore = require('session-file-store')(session)
+const http = require('http')
+const helpers = require('./app/helpers')
+const passport = require('passport')
+const Auth0Strategy = require('passport-auth0')
+const Config = require('./app/classes/config')
+
+//  Read in the config file
+const config = new Config()
+
+const app = express()
+const hbs = exphbs.create({
+  extname: '.html',
+  helpers,
+  partialsDir: `${__dirname}/app/templates/includes/`
+})
+
+app.engine('html', hbs.engine)
+app.set('view engine', 'html')
+app.set('views', `${__dirname}/app/templates`)
+app.use(
+  express.static(`${__dirname}/app/public`, {
+    'no-cache': true
+  })
+)
+app.use(bodyParser.json())
+app.use(
+  bodyParser.urlencoded({
+    extended: true
+  })
+)
+app.use(cookieParser())
+app.use(
+  session({
+    // Here we are creating a unique session identifier
+    secret: config.get('handshake'),
+    resave: true,
+    saveUninitialized: true,
+    store: new FileStore({
+      ttl: 60 * 60 * 24 * 7
+    })
+  })
+)
+const auth0 = config.get('auth0')
+if (auth0 !== null) {
+  // Configure Passport to use Auth0
+  const strategy = new Auth0Strategy({
+    domain: auth0.AUTH0_DOMAIN,
+    clientID: auth0.AUTH0_CLIENT_ID,
+    clientSecret: auth0.AUTH0_SECRET,
+    callbackURL: auth0.AUTH0_CALLBACK_URL
+  },
+  (accessToken, refreshToken, extraParams, profile, done) => {
+    return done(null, profile)
+  }
+  )
+
+  passport.use(strategy)
+
+  // This can be used to keep a smaller payload
+  passport.serializeUser(function (user, done) {
+    done(null, user)
+  })
+
+  passport.deserializeUser(function (user, done) {
+    done(null, user)
+  })
+
+  app.use(passport.initialize())
+  app.use(passport.session())
+}
+
+app.use('/', routes)
+
+app.use((request, response) => {
+  response.status(404).render('static/404')
+})
+
+if (process.env.NODE_ENV !== 'DEV') {
+  app.use((err, req, res) => {
+    console.error(err.stack)
+    res.status(500).send('Something broke!')
+  })
+}
+
+//  Check to see if the old pid is active, if so we kill it
+const pidFile = path.join(rootDir, '.pid')
+if (fs.existsSync(pidFile)) {
+  const pid = fs.readFileSync(pidFile, 'utf-8')
+  console.log('old pid: ', pid)
+  const isRunning = require('is-running')(pid)
+  if (isRunning) {
+    process.kill(pid)
+  }
+}
+
+fs.writeFileSync(pidFile, process.pid, 'utf-8')
+
+let skipOpen = false
+if ('skipOpen' in argOptions && argOptions.skipOpen === true) {
+  skipOpen = true
+}
+
+//  If we are on the dev server and we aren't restarting with a
+//  build skip, then start up a browser to get the user going.
+//  If we don't have any Auth0 stuff in place yet we also need
+//  to pass over the handshake value so we can do a quick
+//  basic authentication.
+if (process.env.NODE_ENV === 'development') {
+  if (skipBuild === false && skipOpen === false) {
+    const opn = require('opn')
+    // If there is no auth0 entry in the config file then we need
+    // to pass over the handshake value
+    if (!('auth0' in config)) {
+      opn(
+        `http://${process.env.HOST}:${process.env.PORT}?handshake=${config.handshake}`
+      )
+    } else {
+      opn(`http://${process.env.HOST}:${process.env.PORT}`)
+    }
+  }
+  console.log(`>> Connect to: ${process.env.HOST}:${process.env.PORT}`.alert)
+} else {
+  console.log(
+    `
+>> Welcome to the Dashboard, please visit the site however you have your host and ports setup to see it from the outside world`
+      .info
+  )
+  if (config.get('auth0') === null) {
+    console.log(
+      `>> You will be asked for a 'handshake' code while setting up the next step, please use the following value
+      `.info
+    )
+    console.log(config.get('handshake').bold.warn)
+    console.log('')
+    console.log(
+      '>> You can also find the value in the '.info +
+      'config.json'.bold.data +
+      ' file'.info
+    )
+    console.log('')
+  }
+}
+
+http.createServer(app).listen(process.env.PORT)
+
+//  Now we kick off the regular tasks that do things periodically
+//  kinda like cron jobs
+const pingtools = require('./app/modules/pingtools')
+pingtools.startPingingES()
---
+++ src/routes/index.js
@@ -0,0 +1,149 @@
+const express = require('express')
+const passport = require('passport')
+const router = express.Router()
+const User = require('../classes/user')
+const ensureLoggedIn = require('connect-ensure-login').ensureLoggedIn()
+const Config = require('../classes/config')
+
+// Break out all the seperate parts of the site
+/* eslint-disable import/no-unresolved */
+const config = require('./config')
+const main = require('./main')
+const status = require('./status')
+
+// ############################################################################
+//
+/*
+ * Always create a templateValues object that gets passed to the
+ * templates. The config object from global (this allows use to
+ * manipulate it here if we need to) and the user if one exists
+ */
+//
+// ############################################################################
+router.use(function (req, res, next) {
+  req.templateValues = {}
+  const configObj = new Config()
+  req.config = configObj
+  req.templateValues.config = req.config
+  req.templateValues.NODE_ENV = process.env.NODE_ENV
+  if (req.user === undefined) {
+    req.user = null
+  } else {
+    //  Shortcut the roles
+    if ('user_metadata' in req.user && 'roles' in req.user.user_metadata) {
+      req.user.roles = req.user.user_metadata.roles
+      req.user.apitoken = req.user.user_metadata.apitoken
+    } else {
+      req.user.roles = {
+        isAdmin: false,
+        isDeveloper: false,
+        isStaff: false
+      }
+    }
+  }
+  req.templateValues.user = req.user
+
+  //  If there is no Auth0 setting in config then we _must_
+  //  check to see if we are setting Auth0 settings and if
+  //  not, redirect to the Auth0 form.
+  if (configObj.get('auth0') === null) {
+    // Check to see if values are being posted to us
+    if (req.method === 'POST') {
+      if (
+        'action' in req.body &&
+        'AUTH0_DOMAIN' in req.body &&
+        'AUTH0_CLIENT_ID' in req.body &&
+        'AUTH0_SECRET' in req.body &&
+        'AUTH0_CALLBACK_URL' in req.body &&
+        'handshake' in req.body &&
+        req.body.action === 'save' &&
+        req.body.handshake === configObj.get('handshake')
+      ) {
+        const auth0 = {
+          AUTH0_DOMAIN: req.body.AUTH0_DOMAIN,
+          AUTH0_CLIENT_ID: req.body.AUTH0_CLIENT_ID,
+          AUTH0_SECRET: req.body.AUTH0_SECRET,
+          AUTH0_CALLBACK_URL: req.body.AUTH0_CALLBACK_URL
+        }
+        configObj.set('auth0', auth0)
+        setTimeout(() => {
+          global.doRestart = true
+          process.exit()
+        }, 500)
+        return res.redirect('/wait')
+      }
+    }
+
+    //  If not, check to see if we've been passed a handshake
+    if ('handshake' in req.query) {
+      req.templateValues.handshake = req.query.handshake
+    }
+
+    //  Set up a nice handy default callback if we are developing
+    if (process.env.NODE_ENV === 'development') {
+      req.templateValues.callbackUrl = `http://${process.env.HOST}:${process.env.PORT}/callback`
+    }
+    req.templateValues.NODE_ENV = process.env.NODE_ENV
+    return res.render('config/auth0', req.templateValues)
+  }
+  next()
+})
+
+// ############################################################################
+//
+//  Here are all the main routes
+//
+// ############################################################################
+
+router.get('/', main.index)
+router.get('/config', ensureLoggedIn, config.index)
+router.post('/config', ensureLoggedIn, config.index)
+router.get('/status', ensureLoggedIn, status.index)
+router.get('/status/elasticsearch', ensureLoggedIn, status.elasticsearch)
+router.get('/wait', main.wait)
+
+// ############################################################################
+//
+//  Log in and log out tools
+//
+// ############################################################################
+
+const configObj = new Config()
+if (configObj.get('auth0') !== null) {
+  const auth0Obj = configObj.get('auth0')
+  router.get(
+    '/login',
+    passport.authenticate('auth0', {
+      clientID: auth0Obj.AUTH0_CLIENT_ID,
+      domain: auth0Obj.AUTH0_DOMAIN,
+      redirectUri: auth0Obj.AUTH0_CALLBACK_URL,
+      audience: `https://${auth0Obj.AUTH0_DOMAIN}/userinfo`,
+      responseType: 'code',
+      scope: 'openid profile'
+    }),
+    function (req, res) {
+      res.redirect('/')
+    }
+  )
+
+  // Perform session logout and redirect to homepage
+  router.get('/logout', (req, res) => {
+    req.logout()
+    res.redirect('/')
+  })
+
+  // Perform the final stage of authentication and redirect to '/user'
+  router.get(
+    '/callback',
+    passport.authenticate('auth0', {
+      failureRedirect: '/'
+    }),
+    async function (req, res) {
+      //  Update the user with extra information
+      req.session.passport.user = await new User().get(req.user)
+      res.redirect(req.session.returnTo || '/')
+    }
+  )
+}
+
+module.exports = router
