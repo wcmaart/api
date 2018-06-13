c758d936a3bb3f8d622c37aed442fdeb3c66f011
revdancatt <github@revdancatt.com>
2018-06-13 11:40:25 +0100

    List event ids in object response

--- src/modules/schema/index.js
+++ src/modules/schema/index.js
@@ -13,7 +13,7 @@ type Query {
     color_threshold: Float = 50.0
     color_source: String = "google"
   ): [Object]
-  object(id: Int!): Object
+  object(id: Int!): SingleObject
   objectNames(page: Int, per_page: Int, sort: String = "asc", sort_field: String = "id"): [ObjectName]
   makers(page: Int, per_page: Int, sort: String = "asc", sort_field: String = "id"): [Maker]
   periods(page: Int, per_page: Int, sort: String = "asc", sort_field: String = "id"): [Period]
@@ -82,6 +82,50 @@ type Object {
   copyright_holder: String
   remote: Remote
   color: ColorInfo
+}
+
+type SingleObject {
+  id: Int
+  accession_number: String
+  title: String
+  maker: String
+  ulan: String
+  department: String
+  classification: String
+  culture: String
+  period: String
+  creation_date: String
+  creation_date_earliest: String
+  creation_date_latest: String
+  accession_date: String
+  source_name: String
+  object_name: String
+  medium: String
+  description: String
+  credit_line: String
+  paper_support: String
+  catalogue_raisonne: String
+  portfolio: String
+  signed: String
+  marks: String
+  inscriptions: String
+  filename: String
+  dimensions: String
+  element_type: String
+  width_cm: String
+  height_cm: String
+  depth_cm: String
+  width_in: String
+  height_in: String
+  depth_in: String
+  area_in: String
+  size_s_m_l: String
+  is_3d: String
+  orientation_p_l_s: String
+  copyright_holder: String
+  remote: Remote
+  color: ColorInfo
+  events: [Event]
 }
 
 type Remote {
