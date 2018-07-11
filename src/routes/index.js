2be34a9b05935f4ae47ba959298034652ba62b4c
Ubuntu <ubuntu@ip-172-31-35-76.ec2.internal>
2018-07-11 10:44:30 +0000

    funky double redirect check

--- src/routes/index.js
+++ src/routes/index.js
@@ -40,7 +40,18 @@ router.use(cors({
 //  app.enable('trust proxy')
 //  is set in server.js
 router.use(function (req, res, next) {
-  if (!(req.secure) && process.env.REDIRECT_HTTPS === 'true') {
+  let remoteAccess = true
+
+  //  Because of the way we are hosting we need to do an extra weird check
+  //  about coming in from outside or via a ip:port before we tie up the whole
+  //  lot in a knot.
+  const hostSplit = req.headers['host'].split(':')
+  if (hostSplit.length > 1) {
+    if (hostSplit[1] === process.env.PORT) {
+      remoteAccess = false
+    }
+  }
+  if (!(req.secure) && process.env.REDIRECT_HTTPS === 'true' && remoteAccess === true) {
     var secureUrl = 'https://' + req.headers['host'] + req.url
     res.writeHead(301, {
       Location: secureUrl
